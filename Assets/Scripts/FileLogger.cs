0d99690c0ce10c428d3d96457a1af8b74b7254de
Brett Gildersleeve <bgildersleeve@pandora.com>
2019-07-11 21:55:00 -0700

    Initial Unity Project Creation

---
+++ Assets/Scripts/FileLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileLogger : ILogHandler {
+    private FileStream fileStream;
+    private StreamWriter streamWriter;
+    private ILogHandler defaultLogHandler = Debug.unityLogger.logHandler;
+
+    public FileLogger() {
+        string filePath = Application.persistentDataPath + "/MyLogs.txt";
+        fileStream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        streamWriter = new StreamWriter(fileStream);
+        ReplaceDefaultLogger();
+        Debug.Log("Log file: " + filePath);
+    }
+
+    private void ReplaceDefaultLogger() {
+        Debug.unityLogger.logHandler = this;
+    }
+
+    public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) {
+        streamWriter.WriteLine(String.Format(format, args));
+        streamWriter.Flush();
+        defaultLogHandler.LogFormat(logType, context, format, args);
+    }
+
+    public void LogException(Exception exception, UnityEngine.Object context) {
+        defaultLogHandler.LogException(exception, context);
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+    private static ILogger logger = Debug.unityLogger;
+    private static string TAG = "Game";
+    private FileLogger fileLogger;
+
+    void Start() {
+        fileLogger = new FileLogger();
+        logger.Log(TAG, "Start()");
+    }
+
+    void Update() {
+
+    }
+}
